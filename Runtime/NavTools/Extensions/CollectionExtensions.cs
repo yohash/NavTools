cdbdf6a900f7058636cbadffb3a9a6e2c90771b1
yohash <jdheebl@gmail.com>
2023-01-05 21:37:03 +1300

    Initial push of unity project, namespace files, and upm files

---
+++ Runtime/NavTools/Extensions/CollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Yohash.Tools
+{
+  public static class CollectionExtensions
+  {
+    public static List<T> TryAdd<T>(this List<T> list, T add)
+    {
+      if (list.Contains(add)) return list;
+      list.Add(add);
+      return list;
+    }
+  }
+}
