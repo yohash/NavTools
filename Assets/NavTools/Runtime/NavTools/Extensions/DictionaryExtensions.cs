3fe916ab0dd87c5216395ba107c370f46b98ad72
yohash <jdheebl@gmail.com>
2023-01-05 21:37:03 +1300

    Initial push of unity project, namespace files, and upm files

---
+++ Assets/NavTools/Runtime/NavTools/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Yohash.Tools
+{
+  public static class DictionaryExtensions
+  {
+    public static V TryGetOrDefault<K, V>(this Dictionary<K, V> d, K key, V defaultValue = default(V))
+    {
+      return d.ContainsKey(key) ? d[key] : defaultValue;
+    }
+
+    public static string Serialize<K, V>(this Dictionary<K, V> d)
+    {
+      string s = d.GetType().ToString() + "\n";
+      foreach (var kvp in d) {
+        s += "\t" + kvp.Key.ToString() + " : " + kvp.Value.ToString() + "\n";
+      }
+      return s;
+    }
+  }
+}
---
+++ Assets/NavTools/Runtime/NavTools/Extensions/GameObjectExtensions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Yohash.Tools
+{
+  public static class GameObjectExtensions
+  {
+    public static T GetOrAddComponent<T>(this GameObject go) where T : Component
+    {
+      T component = go.GetComponent<T>();
+      if (component != null) { return component; }
+      component = go.AddComponent<T>();
+      return component;
+    }
+
+    public static void SoftSetActive(this GameObject go, bool activeSelf)
+    {
+      if (go && go.activeSelf != activeSelf) {
+        go.SetActive(activeSelf);
+      }
+    }
+  }
+}
